2840fa0d7da6f60e968e52c7337cb0db093fcc58
Jonas Orrico <jonas@estupendo.net>
2014-08-11 17:41:36 -0300

    Add scrollReveal data- for tests

--- themes/index.php
+++ themes/index.php
@@ -9,7 +9,6 @@
 
   <?php include 'header.php'; ?>
 
-
     <div class="container">
 
       <!-- Jumbotron -->
@@ -21,18 +20,18 @@
 
       <!-- Example row of columns -->
       <div class="row mkt">
-        <div class="col-sm-6 col-md-3 col-lg-3">
+        <div data-scroll-reveal class="col-sm-6 col-md-3 col-lg-3">
           <h2>Safari bug warning!</h2>
           <p class="text-danger">As of v7.0.1, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
         </div>
-        <div class="col-sm-6 col-md-3 col-lg-3">
+        <div data-scroll-reveal="enter left and move 100px over 1.33s" class="col-sm-6 col-md-3 col-lg-3">
           <h2>Heading</h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
        </div>
-        <div class="col-sm-6 col-md-3 col-lg-3">
+        <div data-scroll-reveal="enter from the right after 1s" class="col-sm-6 col-md-3 col-lg-3">
           <h2>Heading</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
           <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
